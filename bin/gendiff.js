cc2f98239871450de751c241ff386d94b9c7ba50
Natali Nikitina <nikitina.n.84@yandex.ru>
2022-01-28 21:49:18 +0500

    modify gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,8 @@
 import { Command } from 'commander/esm.mjs';
 const program = new Command();
 program
+  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 program.parse();
