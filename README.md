4f479570b2d814d12c6484e2296bfba1c92145f8
Natali Nikitina <nikitina.n.84@yandex.ru>
2022-01-30 00:30:01 +0500

    create __fixtures__ and index.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/n1k1t1nat/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/n1k1t1nat/frontend-project-lvl2/actions)
+[![asciicast](https://asciinema.org/a/PkoOKkVbkSSZBKotLEWGNy7cs.svg)](https://asciinema.org/a/PkoOKkVbkSSZBKotLEWGNy7cs)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+import genDiff from '../src/index.js';
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 program.parse();
