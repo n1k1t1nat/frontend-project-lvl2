b59a9eaf87941a032f107b6fbb284a91936a82cc
Natali Nikitina <nikitina.n.84@yandex.ru>
2022-02-23 15:24:13 +0500

    modified bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, { format }) => {
-    console.log(genDiff(filepath1, filepath2, format));
-  });
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  })
 program.parse();
