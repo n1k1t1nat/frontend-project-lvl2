017c304348bb4d9c9a42599ecc455fb8ce9fcf6f
Natali Nikitina <nikitina.n.84@yandex.ru>
2022-02-23 15:29:15 +0500

    modified bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,5 +11,5 @@ program
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2, program.opts().format));
-  })
+  });
 program.parse();
